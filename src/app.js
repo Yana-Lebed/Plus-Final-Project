667f6b988a6355b0fda8896cb1b6a66cb9d07ad9
Yana-Lebed <suzukayas@gmail.com>
2022-10-26 19:42:38 +0300

    up

--- src/app.js
+++ src/app.js
@@ -66,7 +66,6 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
 function getForecast(coordinates) {
   let apiKey = "3a3741f568b40f2a3c9fbb3b60567d17";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
