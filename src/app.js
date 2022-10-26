271c9522b0a78bafb2919fc7339706c6c8ec9351
Yana-Lebed <suzukayas@gmail.com>
2022-10-26 21:06:40 +0300

    up key

--- src/app.js
+++ src/app.js
@@ -64,7 +64,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "3a3741f568b40f2a3c9fbb3b60567d17";
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -96,7 +96,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "3a3741f568b40f2a3c9fbb3b60567d17";
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
